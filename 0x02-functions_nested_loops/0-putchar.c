9d8021e294a7e909f2764a10a5f9ea789180867a
chuksebu <chukwuike.ebuzome@gmail.com>
2023-08-15 14:44:46 -0700

    All

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('G');
+	_putchar('O');
+	_putchar('D');
+	_putchar('S');
+	_putchar('P');
+	_putchar('O');
+	_putchar('W');
+	_putchar('E');
+	_putchar('R');
+	_putchar('\n');
+
+	return (0);
+}
